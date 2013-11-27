34c8574aecba0112b51a1eb1b320a0666a4d4633
Thomas Boerger <tboerger@tbpro.de>
2013-11-27 11:26:42 +0100

    Added more crowbar node definitions for mimir only, got enough ram here

--- roles/mimir.rb
+++ roles/mimir.rb
@@ -20,5 +20,51 @@ run_list(
   "recipe[crowbar]"
 )
 
-default_attributes({})
+default_attributes({
+  "crowbar" => {
+    "nodes" => {
+      "cloudx-node4" => {
+        "action" => %w(define),
+        "memory" => 1024,
+        "cpus" => 4,
+        "vnc" => 5901,
+        "disks" => [
+          {
+            "type" => "file",
+            "source" => "/var/lib/libvirt/images/cloudx-node4.qcow2",
+            "target" => "vda"
+          }
+        ],
+        "interfaces" => [
+          {
+            "type" => "bridge",
+            "source" => "virbr1",
+            "target" => "vnet4"
+          }
+        ]
+      },
+      "cloudx-node5" => {
+        "action" => %w(define),
+        "memory" => 1024,
+        "cpus" => 4,
+        "vnc" => 5901,
+        "disks" => [
+          {
+            "type" => "file",
+            "source" => "/var/lib/libvirt/images/cloudx-node5.qcow2",
+            "target" => "vda"
+          }
+        ],
+        "interfaces" => [
+          {
+            "type" => "bridge",
+            "source" => "virbr1",
+            "target" => "vnet5"
+          }
+        ]
+      }
+    }
+  }
+})
+
 override_attributes({})
